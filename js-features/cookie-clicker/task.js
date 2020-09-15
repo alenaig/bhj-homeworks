cfa7f834a68c6c04de1d694fa5b152dbd0973f0f
Alena Ignatyeva <ignatieva.alenka@gmail.com>
2020-09-15 18:53:44 +0300

    second  commit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,14 +1,19 @@
 const image = document.getElementById("cookie");
 const count = document.getElementById("clicker__counter");
+const speed = document.getElementById("clicker__speed");
+let firstClicked = new Date().getTime();
 
 image.onclick = function() {
     
     count.textContent ++;
-    
+
     if(this.width == 200) {
         this.width = 100;
     }
     else this.width = 200;
 
+    let lastClicked = new Date().getTime();
+    speed.textContent = (lastClicked - firstClicked) / 1000 ;
 }
+
 
